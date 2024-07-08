429cd3b5bbb89baab0d10909d45a2d5d55d62bc9
Pawan Ajjar K <1nc20ci027@ncetmail.com>
2024-07-08 20:50:43 +0530

    add register user

--- app/(dashboard)/app/page.tsx
+++ app/(dashboard)/app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home(){
-    return(
+import RegisterUser from '@/components/RegisterUser/RegisterUser';
+
+export default function Home() {
+    return (
         <main>
-            
+            <h1>Dashboard</h1>
+            <RegisterUser />
         </main>
     )
 }
