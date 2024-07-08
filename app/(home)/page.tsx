ae19dd6b59463716d4d7eb9a35934e0eba6d5483
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-07-08 18:31:26 +0530

    :rotating_light: FAQs

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/components/Header/index";
 import Featured from "@/components/Featured/Featured";
 import HeroSection from "@/components/HeroSection";
-
+import FAQ from "@/components/FAQ";
 
 export default function Home(){
     return(
         <main>
             <HeroSection />
             <Featured />
+            <FAQ />
         </main>
     )
 }
--- data/index.ts
+++ data/index.ts
@@ -49,4 +49,48 @@ export const mobileParagraphPhrases = [
       },
     },
   };
+
+
+  type FAQItem = {
+    question: string;
+    answer: string;
+  };
+  
+  export const desktopHeaderPhrase = ['Frequently asked', 'questions'];
+  export const mobileHeaderPhrase = ['Frequently', 'asked', 'questions'];
+  export const animate = {
+    initial: {
+      y: '100%',
+      opacity: 0,
+    },
+    open: (i: number) => ({
+      y: '0%',
+      opacity: 1,
+      transition: { duration: 1, delay: 0.1 * i, ease: [0.33, 1, 0.68, 1] },
+    }),
+  };
+  
+  export const faqData: FAQItem[] = [
+    {
+      question: 'Why do I use Griffins?',
+      answer:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      question: 'How does a Game Developer Earn from Griffin?',
+      answer:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      question: 'What types of transactions can I perform with Griffin?',
+      answer:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+      question: 'What benefits does Griffin offer over other platforms?',
+      answer:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+  ];
+  
   
